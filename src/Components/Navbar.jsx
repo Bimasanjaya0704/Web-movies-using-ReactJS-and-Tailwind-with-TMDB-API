c62d8c22d3e261af60b7e4334211b16b879c0b03
Bimasanjaya0704 <sanjabim77@gmail.com>
2023-05-31 00:45:24 +0700

    update navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,80 +1,87 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Search from "./Search";
 
-export default function NavBar() {
-    const [navbar, setNavbar] = useState(false);
-
-    return (
-        <nav className="w-full bg-dark shadow">
-            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-2">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <a href="javascript:void(0)">
-                            <h2 className="text-xl font-bold lg:text-4xl text-primary ">Web Movies</h2>
-                        </a>
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-primary rounded-md outline-none focus:border-primary focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                            navbar ? "block" : "hidden"
-                        }`}
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <nav className="w-full bg-dark shadow">
+      <div className="px-4">
+        <div className="justify-between mx-auto lg:items-center lg:flex lg:px-2 ">
+          <div>
+            <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
+              <a href="#">
+                <h2 className="text-xl font-bold md:text-3xl lg:text-4xl text-primary ">
+                  BIMovies
+                </h2>
+              </a>
+              <div className="lg:hidden">
+                <button
+                  className="p-2 text-primary rounded-md outline-none focus:border-primary focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
                     >
-                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            <li className="text-light hover:text-primary">
-                                <a href="javascript:void(0)">Home</a>
-                            </li>
-                            <li className="text-light hover:text-primary">
-                                <a href="javascript:void(0)">Upcoming</a>
-                            </li>
-                            <li className="text-light hover:text-primary">
-                                <a href="javascript:void(0)">Popular</a>
-                            </li>
-                            <li className="text-light hover:text-primary">
-                                <a href="javascript:void(0)">Top Rated</a>
-                            </li>
-                            <label for="search">
-                                <input type="search" name="search" id="search" placeholder="Cari film !" className="font-normal text-base my-6 px-2 py-1 rounded-md shadow-md text-second focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary"/>
-                            </label>
-                        </ul>
-                    </div>
-                </div>
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
-        </nav>
-    );
-}
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
+                navbar ? "block" : "hidden"
+              }`}
+            >
+              <ul className="items-center justify-center space-y-8 lg:flex lg:space-x-6 xl:space-x-10 lg:space-y-0">
+                <li className="text-light hover:text-primary flex justify-center">
+                  <a href="#">Home</a>
+                </li>
+                <li className="text-light hover:text-primary flex justify-center">
+                  <a href="#">Now Playing</a>
+                </li>
+                <li className="text-light hover:text-primary flex justify-center">
+                  <a href="#">Upcoming</a>
+                </li>
+                <li className="text-light hover:text-primary flex justify-center">
+                  <a href="#">Popular</a>
+                </li>
+                <li className="text-light hover:text-primary flex justify-center">
+                  <a href="#">Top Rated</a>
+                </li>
+                <Search />
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
